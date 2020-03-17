d9f27811891b70142ed896c62a82cb05942fe268
celso.sales <celso.sales@fortbrasil.com.br>
2020-03-17 20:00:25 -0300

    fails try

--- Server/src/implementation.js
+++ Server/src/implementation.js
@@ -1,39 +1,42 @@
-
-const {addPlayer,findPlayers, updatePlayers} = require('../data/players.js');
-const {addMessage,findMessages,getMessages} = require('../data/messages.js');
-const {setStateGame} = require('../data/game.js');
-
+const { addPlayer, findPlayers, updatePlayers } = require("../data/players.js");
+const {
+  addMessage,
+  findMessages,
+  getMessages
+} = require("../data/messages.js");
+const { setStateGame } = require("../data/game.js");
 
 module.exports = {
   CreatePlayer(call, callback) {
-    const {player} = call.request;
-    const responsePlayer =  addPlayer(player);
-    //pegar dados no user me algum estado aqui no server. 
-    return callback(null, responsePlayer)
+    const { player } = call.request;
+    const responsePlayer = addPlayer(player);
+    console.log("bateu aqui");
+    //pegar dados no user me algum estado aqui no server.
+    return callback(null, responsePlayer);
   },
   FindPlayers(call, callback) {
-    const players = findPlayers()
-    return callback(null, {players})
+    const players = findPlayers();
+    return callback(null, { players });
   },
   UpdatePlayersServer(call, callback) {
-    const {players} = call.request;
+    const { players } = call.request;
     const responsePlayers = updatePlayers(players);
-    return callback(null, responsePlayers)
+    return callback(null, responsePlayers);
   },
   CreateMessage(call, callback) {
-    const {message} = call.request;
+    const { message } = call.request;
     addMessage(message);
-    //pegar dados no user me algum estado aqui no server. 
-    return callback(null, {message})
+    //pegar dados no user me algum estado aqui no server.
+    return callback(null, { message });
   },
   FindMessages(call, callback) {
-    const messages = findMessages()
-    return callback(null, {messages})
+    const messages = findMessages();
+    return callback(null, { messages });
   },
   UpdateGameState(call, callback) {
-    const {gamestate} = call.request;
+    const { gamestate } = call.request;
     setStateGame(gamestate);
-    //pegar dados no user me algum estado aqui no server. 
-    return callback(null, {gamestate})
-  },
-}
+    //pegar dados no user me algum estado aqui no server.
+    return callback(null, { gamestate });
+  }
+};
